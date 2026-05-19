4535d803148bedc516118ce6ddee417dbd968d77
thomastokmakov24 <thomast89@nycstudents.net>
2026-05-19 12:31:16 -0400

    21
    help gng

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,22 +1,32 @@
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from '../utils/supabase'
-  
-  const todos = ref([])
+<template>
+  <p v-if="error">Error: {{ error }}</p>
+    <ul v-else>
+      <li v-for="transaction in transactions" :key="transaction.id">
+        ID: {{ transaction.id }} | User: {{ transaction.user_id }} | ...
+      </li>
+    </ul>  
+</template>
 
-  async function getTodos() {
-    const { data } = await supabase.from('todos').select()
-    todos.value = data
+<script setup >
+import { ref, onMounted } from 'vue'
+import { supabase } from './supabase'
+
+const transactions = ref([])
+const error = ref(null)
+
+onMounted(async () => {
+  let { data: transactionData, error: err } = await supabase
+    .from('transactions')
+    .select('*')
+  if (err) {
+    error.value = err.message
+  } else {
+    transactions.value = transactionData
   }
-
-  onMounted(() => {
-    getTodos()
-  })
+})
 
 </script>
 
-<template>
-  <ul>
-    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
-  </ul>
-</template>
+<style scoped>
+
+</style>
